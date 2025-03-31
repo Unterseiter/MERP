5aace71f7acb0c7f396aa2159474eec38acb29c4
nikonline-oss <151464633+nikonline-oss@users.noreply.github.com>
2025-03-31 12:18:42 +0300

    добавил ещё один евент в карусель

--- event-platform-frontend/src/components/carousel/carousel.jsx
+++ event-platform-frontend/src/components/carousel/carousel.jsx
@@ -14,6 +14,12 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
       description: "Описание новогоднего мероприятия...",
       imageUrl: "https://images.unsplash.com/photo-1482517967863-00e15c9b44be?auto=format&fit=crop&q=80&w=1000",
     },
+    {
+      id: 3,
+      title: "Детский дом ищет детей",
+      description: "Супер детский дом, кормят бесплатно",
+      imageUrl: "https://avatars.mds.yandex.net/i?id=a8d7a36e2b416aa525797979c42327fe04aa9a67-8497409-images-thumbs&n=13",
+    },
   ];
 
 const Carousel = () => {
--- event-platform-frontend/src/components/date/test-date.js
+++ event-platform-frontend/src/components/date/test-date.js
@@ -17,6 +17,18 @@ export const events = [
       description: "Ищем активных людей для совместного отдыха и путешествий в тыл врага",
       imageUrl: "https://images.unsplash.com/photo-1579912437766-7896df6d3cd6?auto=format&fit=crop&q=80&w=1000",
     },
+    {
+      id: 4,
+      title: "Ищу партию для решения математики",
+      description: "5 человек, Это супер математика",
+      imageUrl: "https://avatars.dzeninfra.ru/get-zen_doc/244664/pub_63a66902a88640098fc19587_63a66a40e7cb570eeb6331c4/scale_1200",
+    },
+    {
+      id: 5,
+      title: "Компания котику",
+      description: "Я котик",
+      imageUrl: "https://avatars.mds.yandex.net/i?id=3ac961fe146c7eb7bda81a2fab9119f60bb9efec-4055263-images-thumbs&n=13",
+    }
   ];
 
 export const events2 = [
